a1858501ecc1e5cc2437bf141b5ab6be4433ff7d
xybai0103 <xueyin0103@gmail.com>
2023-04-24 11:15:58 -0500

    create associations between different tables in the index.js file in the models folder

--- models/index.js
+++ models/index.js
@@ -5,12 +5,39 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  // Define an alias for when data is retrieved
+  //*when Product data is retrieved, the related Tag data will be under the product_tags property
+  as: 'product_tags'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  // Define an alias for when data is retrieved
+  //*when Tag data is retrieved, the related Product data will be under the tag_products property
+  as: 'tag_products'
+});
 
 module.exports = {
   Product,
